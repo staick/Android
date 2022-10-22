7def28b55e99a82fb2f2944db1a80421db7dd22b
Staick <staick@163.com>
2022-10-22 23:31:57 +0800

    My first android app.

---
+++ MyFirstApp/app/src/main/java/dev/staick/myfirstapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package dev.staick.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.myButton)
+        val tvMyTextView = findViewById<TextView>(R.id.textView)
+        var timesClicked = 0
+        btnClickMe.setOnClickListener {
+            timesClicked++
+            tvMyTextView.text = timesClicked.toString()
+            Toast.makeText(this, "Hey Staick!", Toast.LENGTH_LONG).show()
+        }
+    }
+}
